6fa6b15c95c0e423feccc134a29ce023373c61be
shreyak22 <contact.shreya22@gmail.com>
2019-04-21 23:38:19 -0500

    completed assigment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:12356790@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'wertyuiop1234'
 
 
 class Post(db.Model):
@@ -29,15 +30,17 @@ def index():
     return render_template('index.html',title="Build A Blog!", posts=posts)
 
 
-@app.route('/add', methods=['POST', 'GET'])
+@app.route('/newpost', methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
         title_name = request.form['title']
-        post = request.form['post']
+        post = request.form['post']   
+        if title_name == "" or post == "":
+            return render_template('add.html', title_error="This field is required", post_value=post, title_value=title_name)
         new_post = Post(title_name, post)
         db.session.add(new_post)
         db.session.commit()
-        return render_template('add-post.html')
+        return render_template('add-post.html', post=new_post)
 
     
     return render_template('add.html')
@@ -52,7 +55,7 @@ def post_title():
         
     post_id = int(request.args.get('id'))
     post = Post.query.get(post_id)   
-    return render_template('post-title.html',post=post)
+    return render_template('add-post.html',post=post)
 
 
 if __name__ == '__main__':
